df91dee3624e5f6b0b99346816906817179a5b29
俊 汪 <turingai@outlook.com>
2019-08-06 00:54:07 +0800

    添加作业基本程序

---
+++ WangJun.Job/WangJunJob.cs
@@ -0,0 +1,66 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Threading.Tasks;
+
+namespace WangJun.Yun
+{
+    public class WangJunJob : IJob
+    {
+        protected StdSchedulerFactory factory  ;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static WangJunJob GetInst()
+        {
+            var inst = new WangJunJob();
+            inst.factory = new StdSchedulerFactory();
+            return inst;
+        }
+
+        public RES StartJob(string jobName,string triggerName,int interval , int repeatCount)
+        {
+            try
+            {
+                 
+                var scheduler = this.factory.GetScheduler();
+
+                var jobDetail = JobBuilder.Create<WangJunJob>().WithIdentity(jobName).Build();
+                var trigger = TriggerBuilder.Create().WithIdentity(triggerName).WithSimpleSchedule(p => p.WithIntervalInSeconds(interval).WithRepeatCount(repeatCount)).Build();
+                scheduler.Result.ScheduleJob(jobDetail, trigger);
+                scheduler.Result.Start();
+                return RES.OK();
+            }
+            catch (Exception ex)
+            {
+                return RES.FAIL(ex.Message, ex.Message);
+            }
+        }
+
+        public RES StopJob(string jobName)
+        {
+            try
+            {
+                var scheduler = this.factory.GetScheduler();
+                scheduler.Result.DeleteJob(new JobKey(jobName));
+                return RES.OK();
+            }
+            catch (Exception ex)
+            {
+                return RES.FAIL(ex.Message, ex.Message);
+            }
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            var task = new Task(() =>
+            {
+                Console.WriteLine($"Greetings from HelloJob! {context.RefireCount}");
+            });
+            task.Start();
+            return task;
+        }
+    }
+}
---
+++ WangJun.JobProcessor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WangJun.Yun;
+
+namespace WangJun.JobProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WangJunJob.GetInst().StartJob("Job1","T1",3,10);
+            Task.Run(() =>
+            {
+                System.Threading.Thread.Sleep(20*1000);
+                WangJunJob.GetInst().StopJob("Job1");
+            });
+            Console.ReadKey();
+        }
+    }
+}
